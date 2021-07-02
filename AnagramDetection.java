a941c4d17ffe12c4c315122518f0422d8fa17eb9
pranav21999 <pranavdani99@gmail.com>
2021-07-03 00:24:41 +0530

    Added program to check two Strings are  Anagram or not

---
+++ AnagramDetection.java
@@ -0,0 +1,48 @@
+package algorithm;
+
+import java.io.*;
+import java.util.*;
+
+ 
+public class AnagramDetection {
+ 
+    /* method to check whether two strings are
+    anagram of each other */
+    static boolean areAnagram(char[] str1, char[] str2)
+    {
+        // Get lenghts of both strings
+        int n1 = str1.length;
+        int n2 = str2.length;
+ 
+        // If length of both strings is not same,
+        // then they cannot be anagram
+        if (n1 != n2)
+            return false;
+ 
+        // Sort both strings
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+ 
+        // Compare sorted strings
+        for (int i = 0; i < n1; i++)
+            if (str1[i] != str2[i])
+                return false;
+ 
+        return true;
+    }
+ 
+    // main method
+    public static void main(String args[])
+    {
+        char str1[] = { 'a', 'b', 'c', 'd' };
+        char str2[] = { 'd', 'b', 'c', 'a' };
+       
+        // Function Call
+        if (areAnagram(str1, str2))
+            System.out.println("The two strings are"
+                               + " anagram of each other");
+        else
+            System.out.println("The two strings are not"
+                               + " anagram of each other");
+    }
+}
